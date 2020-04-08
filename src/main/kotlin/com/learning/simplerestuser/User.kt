efbb02f0291aed995297981826a23773b5be240d
Rames <rames.p@gmail.com>
2020-04-08 19:30:18 +0530

    initial checkin

---
+++ src/main/kotlin/com/learning/simplerestuser/User.kt
@@ -0,0 +1,3 @@
+package com.learning.simplerestuser
+
+data class User (var name:String, var age:Int)
---
+++ src/main/kotlin/com/learning/simplerestuser/UserResController.kt
@@ -0,0 +1,18 @@
+package com.learning.simplerestuser
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserResController {
+
+    @RequestMapping("/")
+    fun getDefault(): String{
+        return "Welcome to test app"
+    }
+
+    @RequestMapping("/user")
+    fun getUser(): User{
+        return User("TestUser",12)
+    }
+}
