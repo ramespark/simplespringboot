b512edca86ebf1e1ac677164d5258a6029f59a35
Rames <rames.p@gmail.com>
2020-04-08 21:58:07 +0530

    added script file

--- script/artifact_upload.py
+++ script/artifact_upload.py
@@ -3,8 +3,8 @@ import os
 from botocore.exceptions import NoCredentialsError
 
 def upload_to_aws(local_file, bucket, s3_file):
-    s3 = boto3.client('s3', aws_access_key_id=os.environ['ACCESS_KEY'],
-                      aws_secret_access_key=os.environ['SECRET_KEY'])
+    s3 = boto3.client('s3', aws_access_key_id=os.getenv('ACCESS_KEY'),
+                      aws_secret_access_key=os.getenv('SECRET_KEY'))
 
     try:
         s3.upload_file(local_file, bucket, s3_file)
