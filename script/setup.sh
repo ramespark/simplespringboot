a6b717dfb60eecb4ada22053e416906c501e5602
Rames Palanisamy <rames.p@gmail.com>
2020-04-21 00:25:42 +0530

    copy jar

--- script/setup.sh
+++ script/setup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+scp -o StrictHostKeyChecking=no -i ./listudy.pem ./simplerestuser-0.0.1-SNAPSHOT.jar ubuntu@13.233.114.151:/home/ubuntu/simplerestuser-0.0.1-SNAPSHOT.jar
 ssh -o StrictHostKeyChecking=no -i ./listudy.pem ubuntu@13.233.114.151  sudo /home/ubuntu/setup.sh
