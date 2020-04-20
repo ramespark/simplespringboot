7e0009660d9a831b5af68d67be9af04d273d6f6b
Rames Palanisamy <rames.p@gmail.com>
2020-04-21 00:23:23 +0530

    command

--- script/setup.sh
+++ script/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -i ./listudy.pem ubuntu@13.233.114.151 ssh /home/ubuntu/setup.sh
+ssh -o StrictHostKeyChecking=no -i ./listudy.pem ubuntu@13.233.114.151  sudo /home/ubuntu/setup.sh
